3f7c61afb9ba2629045d17cc3bc4f41a9d78ee3b
dede232 <dede232@yopmail.com>
2016-01-12 15:42:07 +0100

    Etape 3.6

--- app.js
+++ app.js
@@ -26,6 +26,15 @@
 		return this.current === currentGallery;
 	};
   });
+  
+  app.controller('ReviewController', function(){
+    this.review = {};
+    
+    this.addReview = function(product) {
+      product.reviews.push(this.review);
+      this.review = {};
+    };
+  });
 
   var gems = [
     {
