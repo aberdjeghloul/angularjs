7777cb13e9ec6d4b3b1c683a1031592cbb7b71ae
dede232 <dede232@yopmail.com>
2016-01-13 16:12:55 +0100

    4.4

--- app.js
+++ app.js
@@ -43,6 +43,20 @@
     		templateUrl: 'product-description.html'
       };
   });
+  
+  app.directive("productReviews", function() {
+    return {
+      restrict: 'E',
+      templateUrl: "product-reviews.html"
+    };
+  });
+  
+  app.directive("productSpecs", function() {
+    return {
+      restrict: 'A',
+      templateUrl: "product-specs.html"
+    };
+  });  
 
   var gems = [
     {
